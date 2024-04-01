5a2aa3a7e6aef32edd8b9810264d680a421876a3
MrKhalid <abkhalid291@gmail.com>
2024-04-01 16:01:39 +0530

    liquibase project it hase controller to creatr user, user jpa repository,controller should have all curd operation.

---
+++ src/main/java/com/example/liquibasedemo/LiquibasedemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.liquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibasedemoApplication.class, args);
+        System.out.println("khalid you done it......");
+    }
+
+}
